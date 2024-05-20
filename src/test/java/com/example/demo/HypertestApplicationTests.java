1533f418b63979a40b6d6ef30c9f78e573b84d92
sudeep-Log <sudeepghosal3@gmail.com>
2024-05-20 13:24:28 +0530

    initial commit

---
+++ src/test/java/com/example/demo/HypertestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HypertestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
